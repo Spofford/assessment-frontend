3789b369401ac9c994feea24d3a56a30b2a4ed1b
amhasler <amhasler@gmail.com>
2017-12-17 11:36:34 -0500

    assessment body ready

--- app/components/Assessment/index.js
+++ app/components/Assessment/index.js
@@ -2,6 +2,7 @@ import { connect } from "react-redux"
 import React from "react"
 import cssModules from "react-css-modules"
 import style from "./style.css"
+import { default as Header } from "../Header"
 
 const mapStateToProps = state => ({
   user: state.user
@@ -15,7 +16,12 @@ export class Assessment extends React.Component {
   render() {
     if (this.props.user.username) {
       return (
-        <h1>Assessment</h1>
+        <div className={style.container}>
+          <Header />
+          <div className={style.leader}>
+            <h1 className={style.title}>Hi {this.props.user.username}</h1>
+          </div>
+        </div>
       )
     }
     return (
